d50ed4e765b9cb85f5a453293701b33ecde4f6dd
Derek Musselmann <derek@disflux.com>
2013-08-28 18:26:22 -0500

    Track more data from Amazon

--- products/amazon_utils.py
+++ products/amazon_utils.py
@@ -13,6 +13,17 @@ def get_or_create_product(asin):
         product = Product(asin=asin, upc=az.upc, ean=az.ean, 
                           description=az.title, image_url=az.large_image_url,
                           amazon_url=az.offer_url)
+    
+    product.manufacturer = az.get_attribute('Manufacturer')
+    product.brand = az.get_attribute('Brand')
+    product.model_number = az.get_attribute('Model')
+    product.mpn = az.get_attribute('MPN')
+    product.sku = az.sku
+    product.isbn = az.isbn
+    product.length = az.get_attribute('ItemDimensions.Length')
+    product.width = az.get_attribute('ItemDimensions.Width')
+    product.height = az.get_attribute('ItemDimensions.Height')
+    product.weight = az.get_attribute('ItemDimensions.Weight')
     product.save()
         
     return product  
--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from elaphe import barcode
 from StringIO import StringIO
 import random
 import string
@@ -25,6 +26,13 @@ class Product(models.Model):
     asin = models.CharField(max_length=10, null=True)
     brand = models.CharField(max_length=128, null=True)
     manufacturer = models.CharField(max_length=128, null=True)
+    mpn = models.CharField(max_length=32, null=True)
+    sku = models.CharField(max_length=32, null=True)
+    model_number = models.CharField(max_length=64, null=True)
+    length = models.IntegerField(null=True)
+    width = models.IntegerField(null=True)
+    height = models.IntegerField(null=True)
+    weight = models.IntegerField(null=True)
     description = models.CharField(max_length=512, null=True)
     image_url = models.CharField(max_length=512, null=True)
     amazon_url = models.URLField(null=True)
@@ -34,40 +42,42 @@ class Product(models.Model):
         self.onemg = onemg_generator()
         super(Product, self).save(*args, **kwargs)
 
-    def generate_barcode(self, type='ean13', text=True):
-        fp = StringIO()
+    def __unicode__(self):
+        return "%s : %s" % (self.onemg, self.description)
 
-        from elaphe import barcode
-        if type == 'ean13':
-            barcode_text = self.ean
-            code_options = dict(includetext=text, textfont='Courier New',
-                                       textsize=12, showborder=False)
-            margin=1
-            scale=1.25
-            data_mode=None
-        if type == 'upca':
-            barcode_text = self.upc
-            code_options = dict(includetext=text, textfont='Courier New',
-                                       textsize=12, showborder=False)
-            margin=1
-            scale=1.25
-            data_mode = None
-        if type == 'qrcode':
-            barcode_text = self.amazon_url
-            code_options = dict(version=9, eclevel='M')
-            margin=10
-            scale = 1
-            data_mode='8bits'
-        
-        barcode = barcode(type, str(barcode_text), options=code_options, margin=margin, scale=scale, data_mode=data_mode)
-        
-        barcode.save(fp, format='PNG')
-        file = InMemoryUploadedFile(fp, None, "%s-%s.png" % (self.onemg, type),
-                                    'image/png', fp.len, None)
+    def generate_barcode(self, type='ean13', text=True):
         barcode_type = BarcodeType.objects.get(name=type.upper())
         try:
             obj = Barcode.objects.get(product=self, type=barcode_type)
         except:
+            fp = StringIO()
+
+            if type == 'ean13':
+                barcode_text = self.ean
+                code_options = dict(includetext=text, textfont='Courier New',
+                                           textsize=12, showborder=False)
+                margin=1
+                scale=1.25
+                data_mode=None
+            if type == 'upca':
+                barcode_text = self.upc
+                code_options = dict(includetext=text, textfont='Courier New',
+                                           textsize=12, showborder=False)
+                margin=1
+                scale=1.25
+                data_mode = None
+            if type == 'qrcode':
+                barcode_text = self.amazon_url
+                code_options = dict(version=9, eclevel='M')
+                margin=10
+                scale = 1
+                data_mode='8bits'
+            
+            img = barcode(type, str(barcode_text), options=code_options, margin=margin, scale=scale, data_mode=data_mode)
+            
+            img.save(fp, format='PNG')
+            file = InMemoryUploadedFile(fp, None, "%s-%s.png" % (self.onemg, type),
+                                        'image/png', fp.len, None)
             obj = Barcode(product=self, type=barcode_type)
             obj.image.save(file.name, file)
             obj.save()
@@ -83,8 +93,13 @@ class Barcode(models.Model):
     class Meta:
         unique_together = ('product', 'type')
 
+    
+
 class BarcodeType(models.Model):
     name = models.CharField(max_length=32)
+
+    def __unicode__(self):
+        return self.name
 
 
 
